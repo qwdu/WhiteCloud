6792202198850e864b01e813c99c50706909f214
kexue <you@example.com>
2018-03-23 14:46:58 +0800

    fix start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ rm -f storage
 ln -sf $DATA_PATH storage
 
 mkdir -p storage/tmp
-mkdir -p storage/home/admin
-[ -f storage/home/admin/.config.json ] || cp -f user.admin.json storage/home/admin/
+[ -e storage/home/admin ] || mkdir -p storage/home/admin
+[ -f storage/home/admin/.config.json ] || cp -f user.admin.json storage/home/admin/.config.json
 
 node app.js
